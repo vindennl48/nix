24e519d52f245a02d616e99f5ae7eed2a1b5abf3
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2025-02-05 08:25:32 -0500

    Removed hardware.graphics from nvidia module

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,11 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
-
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
